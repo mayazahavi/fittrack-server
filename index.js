e948ac0fc9a2e325809d486765cc2d5015315cb9
mayazahavi <mayazahavi3@gmail.com>
2025-07-30 23:00:06 +0300

    🛠️ Update server entry point and package configuration

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const dotenv = require("dotenv");
 const path = require("path");
-dotenv.config({ path: path.resolve(__dirname, "../.env") });
+require("dotenv").config();
+
 
 const express = require("express");
 const cors = require("cors");
